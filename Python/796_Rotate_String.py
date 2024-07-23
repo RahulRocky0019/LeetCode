4e8f29f048fbc5f0aa670a8ff2be3934c3694b24
Rahul Mullapudi <rahul.rocky0019@gmail.com>
2024-07-23 10:19:40 +0530

    Create 796_Rotate_String.py

---
+++ Python/796_Rotate_String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        n = len(s)
+        for i in range(n):
+            if goal == s[i:] + s[:i]:
+                return True
+        
+        return False
